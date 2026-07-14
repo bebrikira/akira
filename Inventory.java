ce57999f7e5118265f80eeb9dc327546fa680a0d
Kira <bebrikira@ukr.net>
2026-07-14 19:23:12 +0300

    Add Inventory class to manage products

---
+++ Inventory.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inventory {
+
+    private List<Product> products =
+            new ArrayList<>();
+
+    public void add(Product product) {
+
+        products.add(product);
+
+    }
+
+    public List<Product> getProducts() {
+
+        return products;
+
+    }
+
+}
